c70d8e2577614e38b7c2168a8601742f85b92ed4
patrick <blockchainbuddha@gmail.com>
2022-06-13 23:52:37 -0700

    Add prerender

--- web/src/components/AppUrlListener/AppUrlListener.tsx
+++ web/src/components/AppUrlListener/AppUrlListener.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react'
 import { navigate } from '@redwoodjs/router'
 import { App, URLOpenListenerEvent } from '@capacitor/app'
+import { isBrowser } from '@redwoodjs/prerender/browserUtils'
 
 const checkAppLaunchUrl = async () => {
   const { url } = await App.getLaunchUrl()
@@ -10,22 +11,24 @@ const checkAppLaunchUrl = async () => {
 }
 import { Device } from '@capacitor/device'
 
-App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
-  // Example url: https://beerswift.app/tabs/tab2
-  // slug = /tabs/tab2
-  console.log('appUrlOpen event')
-  const slug = event.url.split('.app').pop()
-  if (slug) {
-    let url = new URL(slug)
-    let pathandQuery = url.pathname + url.search
-    console.log(pathandQuery)
-    navigate(pathandQuery)
-    window.location.reload() // Required to trigger the useEffect
-  }
-})
-App.addListener('appStateChange', (event) => {
-  console.log('appStateChange:', JSON.stringify(event))
-})
+isBrowser &&
+  App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+    // Example url: https://beerswift.app/tabs/tab2
+    // slug = /tabs/tab2
+    console.log('appUrlOpen event')
+    const slug = event.url.split('.app').pop()
+    if (slug) {
+      let url = new URL(slug)
+      let pathandQuery = url.pathname + url.search
+      console.log(pathandQuery)
+      navigate(pathandQuery)
+      window.location.reload() // Required to trigger the useEffect
+    }
+  })
+isBrowser &&
+  App.addListener('appStateChange', (event) => {
+    console.log('appStateChange:', JSON.stringify(event))
+  })
 const AppUrlListener: React.FC<any> = () => {
   useEffect(() => {
     checkAppLaunchUrl()
